ad8a2960f998dc66250b7f8744ce70a514aa311f
Preeti Vadali <preeti.abhiram@gmail.com>
2024-10-22 00:19:23 +0200

    created if else statement to check for password with atleast 8 characters and without space

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
  function isValidPassword(username, password){
-    
- }
-    
+    if (password.length < 8){
+       return false;
+    }
+    if (password.includes(" ")) {
+        return false;
+    }
+   else return true;
+}
+console.log(isValidPassword("use12345" , "pass1234"));
